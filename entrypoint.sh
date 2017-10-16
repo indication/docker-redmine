70d09fa2b238463b1dd099fdb28b959a39e2c6e5
in dow <in_dow@hotmail.com>
2017-10-17 02:54:36 +0900

    Fix samba settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ PUBLICNAME=${PUBLICNAME:-$PUBLICFOLDER}
 PRIVATENAME=${PRIVATENAME:-$PRIVATEFOLDER}
 
 set -x
+id
 
 # set config supervisord
 if [ ! -f "/config/supervisord.conf" ]; then
@@ -31,6 +32,26 @@ stderr_logfile_maxbytes=0
 EOF
 fi
 
+# Fix nameserver
+echo -e "search ${SAMBA_REALM}\nnameserver 127.0.0.1" > /etc/resolv.conf
+
+#ad setup
+# Require $SAMBA_REALM to be set
+: "${SAMBA_REALM:?SAMBA_REALM needs to be set}"
+
+# If $SAMBA_PASSWORD is not set, generate a password
+SAMBA_PASSWORD=${SAMBA_PASSWORD:-`(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20; echo) 2>/dev/null`}
+echo "[INFO]Samba password set to: $SAMBA_PASSWORD"
+
+# Populate $SAMBA_OPTIONS
+SAMBA_OPTIONS=${SAMBA_OPTIONS:-}
+
+[ -n "$SAMBA_DOMAIN" ] \
+    && SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=$SAMBA_DOMAIN" \
+    || SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=${SAMBA_REALM%%.*}"
+
+[ -n "$SAMBA_HOST_IP" ] && SAMBA_OPTIONS="$SAMBA_OPTIONS --host-ip=$SAMBA_HOST_IP"
+
 # set config samba
 if [ ! -f "$SHARECONFIG" ]; then
 cat <<EOF>> $SHARECONFIG
@@ -40,7 +61,8 @@ cat <<EOF>> $SHARECONFIG
     passdb backend = samba4
     idmap_ldb:use rfc2307 = yes
     netbios name = $HOSTNAME
-    workgroup = $WORKGROUP
+    ;workgroup = $WORKGROUP
+    workgroup = ${SAMBA_REALM%%.*}
     server string = Samba %v in an Alpine Linux Docker container
     security = user
     guest account = nobody
@@ -65,28 +87,11 @@ cat <<EOF>> $SHARECONFIG
     read only = No
 EOF
 
-# bulk
-
-#ad setup
-# Require $SAMBA_REALM to be set
-: "${SAMBA_REALM:?SAMBA_REALM needs to be set}"
-
-# If $SAMBA_PASSWORD is not set, generate a password
-SAMBA_PASSWORD=${SAMBA_PASSWORD:-`(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20; echo) 2>/dev/null`}
-echo "[INFO]Samba password set to: $SAMBA_PASSWORD"
-
-# Populate $SAMBA_OPTIONS
-SAMBA_OPTIONS=${SAMBA_OPTIONS:-}
-
-[ -n "$SAMBA_DOMAIN" ] \
-    && SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=$SAMBA_DOMAIN" \
-    || SAMBA_OPTIONS="$SAMBA_OPTIONS --domain=${SAMBA_REALM%%.*}"
-
-[ -n "$SAMBA_HOST_IP" ] && SAMBA_OPTIONS="$SAMBA_OPTIONS --host-ip=$SAMBA_HOST_IP"
 
 # Provision domain
-rm -f /etc/samba/smb.conf
+#rm -f /etc/samba/smb.conf
 samba-tool domain provision \
+    --configfile=$SHARECONFIG \
     --use-rfc2307 \
     --realm=${SAMBA_REALM} \
     --adminpass=${SAMBA_PASSWORD} \
