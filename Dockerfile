44a89dabe3dc1e84dd9c7fc2b558e8a60f36aced
in dow <in_dow@hotmail.com>
2017-10-23 03:31:51 +0900

    Fix samba setups

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ LABEL Description="Simple and lightweight Samba docker container, based on Alpin
 RUN apk add --no-cache samba-dc krb5 supervisor
 
 # create a dir for the config and the share
-RUN mkdir /config /share
+RUN mkdir /share
 
 # copy start script
 COPY entrypoint.sh /entrypoint.sh
@@ -14,10 +14,25 @@ RUN chmod +x /entrypoint.sh
 
 # volume mappings
 #VOLUME ["/config", "/share"]
-VOLUME ["/share"]
+VOLUME ["/share","/var/lib/samba"]
 
 # exposes samba's default ports (137, 138 for nmbd and 139, 445 for smbd)
-EXPOSE 137/udp 138/udp 139 445 389
+# DNS*                    53    tcp/udp
+# Kerberos                88    tcp/udp
+# End Point Mapper       135    tcp
+# NetBIOS Name Service   137    udp
+# NetBIOS Datagram       138    udp
+# NetBIOS Session        139    tcp
+# LDAP                   389    tcp/udp
+# SMB over TCP           445    tcp
+# Kerberos kpasswd       464    tcp/udp
+# LDAPS **               636    tcp
+# Global Catalog        3268    tcp
+# Global Catalog SSL ** 3269    tcp 
+# Dynamic RPC Ports ***  49152-65535     tcp
+#         netlogon to 49143
+#         drsuapi  to 49543
+EXPOSE 53 53/udp 88 88/udp 135 137-138/udp 139 389 389/udp 445 464 464/udp 636 1024 3268 3269 49143 49443 49543
 
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["supervisord", "-c", "/config/supervisord.conf"]
+CMD ["supervisord", "-c", "/share/supervisord.conf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,20 @@
 # value
 SHARECONFIG=/share/smb.conf
 
-USER=${USER:-samba}
-auid=${auid:-1000}
-agid=${agid:-$auid}
 WORKGROUP=${WORKGROUP:-WORKGROUP}
 HOSTNAME=$(hostname -s)
 
-PUBLICFOLDER=${PUBLICFOLDER:-data}
-PRIVATEFOLDER=${PRIVATEFOLDER:-private}
-PUBLICNAME=${PUBLICNAME:-$PUBLICFOLDER}
-PRIVATENAME=${PRIVATENAME:-$PRIVATEFOLDER}
-
 set -x
-id
 
 # set config supervisord
-if [ ! -f "/config/supervisord.conf" ]; then
-cat <<EOF>> /config/supervisord.conf
+if [ ! -f "/share/supervisord.conf" ]; then
+cat <<EOF>> /share/supervisord.conf
 [supervisord]
 nodaemon=true
 loglevel=info
 # set some defaults and start samba in foreground (-F), logging to stdout (-S), and using our config (-s path)
 [program:samba]
-command=samba -i -s $SHARECONFIG
+command=samba -i --debuglevel=3 -s $SHARECONFIG
 stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 stderr_logfile=/dev/stderr
@@ -52,6 +43,7 @@ SAMBA_OPTIONS=${SAMBA_OPTIONS:-}
 
 [ -n "$SAMBA_HOST_IP" ] && SAMBA_OPTIONS="$SAMBA_OPTIONS --host-ip=$SAMBA_HOST_IP"
 
+
 # set config samba
 if [ ! -f "$SHARECONFIG" ]; then
 cat <<EOF>> $SHARECONFIG
@@ -59,37 +51,60 @@ cat <<EOF>> $SHARECONFIG
     server role = domain controller
     realm = $SAMBA_REALM
     passdb backend = samba4
-    idmap_ldb:use rfc2307 = yes
+    #idmap_ldb:use rfc2307 = yes
+    
     netbios name = $HOSTNAME
-    ;workgroup = $WORKGROUP
     workgroup = ${SAMBA_REALM%%.*}
+    #template shell = /bin/bash
+    # setup ads
+    dns forwarder = $SAMBA_DNS_FORWARDER
+    #winbind use default domain = true
+    #winbind offline logon = false
+    #winbind nss info = rfc2307
+    #winbind enum users = yes
+    #winbind enum groups = yes
+    #rpc server dynamic port range = 59140-59240
+    #rpc server port:netlogon = 49143
+    #rpc server port:drsuapi  = 49543
+    #rpc server port:dcesrv   = 49443
+
     server string = Samba %v in an Alpine Linux Docker container
-    security = user
-    guest account = nobody
-    map to guest = Bad User
+    #security = user
+    #guest account = nobody
+    #map to guest = Bad User
+    log file = /dev/stdout
     # disable printing services
     load printers = no
-    printing = bsd
-    printcap name = /dev/null
-    disable spoolss = yes
-    # setup ads
-    #dns forwarder = $SAMBA_DNS_FORWARDER
+    #printing = bsd
+    #printcap name = /dev/null
+    #disable spoolss = yes
     # getting rid of those annoying .DS_Store files created by Mac users...
     veto files = /._*/.DS_Store/
     delete veto files = yes
     # support extra stream
     vfs objects = streams_xattr
+    map acl inherit = yes
+    store dos attributes = yes
 [netlogon]
-    path = /var/lib/samba/sysvol/$HOSTNAME/scripts
+    path = /var/lib/samba/sysvol/$SAMBA_REALM/scripts
     read only = No
 [sysvol]
     path = /var/lib/samba/sysvol
     read only = No
+[data]
+    path = /share/data
+    read only = No
 EOF
 
+mkdir /share/data
+chgrp 3000000 /share/data
+chmod 0770 /share/data
 
 # Provision domain
-#rm -f /etc/samba/smb.conf
+rm -rf /etc/krb5.conf
+rm -rf /etc/samba/smb.conf
+rm -rf /var/lib/samba/private/*
+rm -rf /var/lib/samba/sysvol/*
 samba-tool domain provision \
     --configfile=$SHARECONFIG \
     --use-rfc2307 \
@@ -100,56 +115,13 @@ samba-tool domain provision \
     $SAMBA_OPTIONS \
     --option="bind interfaces only"=yes
 
-
-fi
-
-if [[ "$auid" = "0" ]]; then
-       echo "You cant run with root"
-       pause 30
-       exit
-elif id $USER >/dev/null 2>&1; then
-        echo "user exists"
-else
-        echo "user does not exist"
-# add a non-root user and group called "samba" with no password, no home dir, no shell, and gid/uid set to 1000
-addgroup -g $agid $USER && adduser -D -H -G $USER -s /bin/false -u $auid $USER
-# create a samba user matching our user from above with a very simple password ("samba")
-echo -e "$SAMBA_PASSWORD\n$SAMBA_PASSWORD" | smbpasswd -a -s -c $SHARECONFIG $USER
-fi
-
-if [[ ! -z "${PUBLICFOLDER}" ]]; then
-for mnt in "${PUBLICFOLDER}"; do
-  src=$(echo $mnt | awk -F':' '{ print $1 }')
-  if [ ! -d "/share/$src" ]; then mkdir -p /share/$src && chown -R $UID:$GID "/share/$src"; fi
-  cat <<EOF>> $SHARECONFIG
-## share $src
-[$src]
-    comment = $src public folder
-    path = "/share/$src"
-    read only = yes
-    write list = $USER
-    guest ok = yes
-EOF
-done
-fi
-
-if [[ ! -z "${PRIVATEFOLDER}" ]]; then
-for mnt in "${PRIVATEFOLDER}"; do
-  src=$(echo $mnt | awk -F':' '{ print $1 }')
-  if [ ! -d "/share/$src" ]; then mkdir -p /share/$src && chown -R $UID:$GID "/share/$src"; fi
-  cat <<EOF>> $SHARECONFIG
-## share $src
-[$src]
-    comment = $src private folder
-    path = "/share/$src"
-    writeable = yes
-    valid users = $USER
-EOF
-done
-fi
-
-
+echo -----------------------------
+cat $SHARECONFIG
 # show status
 samba-tool domain level show
+fi
+
+rm -f /etc/krb5.conf
+ln -s /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
 exec "$@"
